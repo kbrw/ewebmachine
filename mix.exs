86616a53245100c3649114feb23538546b842c24
Arnaud Wetzel <arnaud.wetzel@kbrwadventure.com>
2014-05-11 00:52:52 +0200

    elixir v0.13

--- mix.exs
+++ mix.exs
@@ -4,8 +4,8 @@ defmodule Ewebmachine.Mixfile do
   def project do
     [ app: :ewebmachine,
       version: "0.0.2",
-      elixirc_paths: ["lib","demo"],        
-      deps: [{:webmachine,">= 1.10",git: "git://github.com/basho/webmachine.git", tag: "1.10.5"}] ]
+      elixirc_paths: ["lib","demo"],
+      deps: [{:webmachine, git: "git://github.com/basho/webmachine.git", tag: "1.10.5"}] ]
   end
 
   def application do
