ef1293da3cb3553320b54f4710a94122eed1ee1a
Arnaud Wetzel <arnaud.wetzel@kbrwadventure.com>
2015-01-13 15:09:04 +0100

    doc as function doc

--- lib/ewebmachine_core.ex
+++ lib/ewebmachine_core.ex
@@ -1,4 +1,3 @@
-
 defmodule Ewebmachine.Core do
   import Ewebmachine.Core.DSL
   import Ewebmachine.Core.API
@@ -7,18 +6,18 @@ defmodule Ewebmachine.Core do
 
   decision v3, do: d(v3b13)
 
-  ## "Service Available"
+  @doc "Service Available"
   decision v3b13, do:
     h(decision_test(resource_call(:ping), :pong, :v3b13b, 503))
   decision v3b13b, do:
     h(decision_test(resource_call(:service_available), true, :v3b12, 503))
-  ## "Known method?"
+  @doc "Known method?"
   decision v3b12, do:
     h(decision_test(h(method) in resource_call(:known_methods),true, :v3b11, 501))
-  ## "URI too long?"
+  @doc "URI too long?"
   decision v3b11, do:
     h(decision_test(resource_call(:uri_too_long), true, 414, :v3b10))
-  ## "Method allowed?"
+  @doc "Method allowed?"
   decision v3b10 do
     methods = resource_call(:allowed_methods)
     if h(method) in methods do
@@ -29,10 +28,10 @@ defmodule Ewebmachine.Core do
     end
   end
   
-  ## "Content-MD5 present?"
+  @doc "Content-MD5 present?"
   decision v3b9, do:
     h(decision_test(h(get_header_val("content-md5")), nil, :v3b9b, :v3b9a))
-  ## "Content-MD5 valid?"
+  @doc "Content-MD5 valid?"
   decision v3b9a do
     case resource_call(:validate_content_checksum) do
       :not_validated ->
@@ -43,10 +42,10 @@ defmodule Ewebmachine.Core do
       _ -> d(v3b9b)
     end
   end
-  ## "Malformed?"
+  @doc "Malformed?"
   decision v3b9b, do:
     h(decision_test(resource_call(:malformed_request), true, 400, :v3b8))
-  ## "Authorized?"
+  @doc "Authorized?"
   decision v3b8 do
     case resource_call(:is_authorized) do
       true -> d(v3b7)
@@ -56,19 +55,19 @@ defmodule Ewebmachine.Core do
     end
   end
       
-  ## "Forbidden?"
+  @doc "Forbidden?"
   decision v3b7, do:
     h(decision_test(resource_call(:forbidden), true, 403, :v3b6))
-  ## "Okay Content-* Headers?"
+  @doc "Okay Content-* Headers?"
   decision v3b6, do:
     h(decision_test(resource_call(:valid_content_headers), true, :v3b5, 501))
-  ## "Known Content-Type?"
+  @doc "Known Content-Type?"
   decision v3b5, do:
     h(decision_test(resource_call(:known_content_type), true, :v3b4, 415))
-  ## "Req Entity Too Large?"
+  @doc "Req Entity Too Large?"
   decision v3b4, do:
     h(decision_test(resource_call(:valid_entity_length), true, :v3b3, 413))
-  ## "OPTIONS?"
+  @doc "OPTIONS?"
   decision v3b3 do
     case h(method) do
       "OPTIONS"->
@@ -78,7 +77,7 @@ defmodule Ewebmachine.Core do
       _ -> d(v3c3)
     end
   end
-  ## Accept exists?
+  @doc "Accept exists?"
   decision v3c3 do
     p_types = for {type,_fun}<-resource_call(:content_types_provided), do: normalize_mtype(type)
     case h(get_header_val("accept")) do
@@ -86,7 +85,7 @@ defmodule Ewebmachine.Core do
       _ -> d(v3c4)
     end
   end
-  ## Acceptable media type available?
+  @doc "Acceptable media type available?"
   decision v3c4 do
     p_types = for {type,_fun}<-resource_call(:content_types_provided), do: normalize_mtype(type)
     case choose_media_type(p_types, h(get_header_val("accept"))) do
@@ -94,24 +93,26 @@ defmodule Ewebmachine.Core do
       type -> h(set_metadata(:'content-type', type)); d(v3d4)
     end
   end
-  ## Accept-Language exists?
+  @doc "Accept-Language exists?"
   decision v3d4, do:
     h(decision_test(h(get_header_val("accept-language")),nil, :v3e5, :v3d5))
-  ## Acceptable Language available? %% WMACH-46 (do this as proper conneg)
+  @doc "Acceptable Language available? %% WMACH-46 (do this as proper conneg)"
   decision v3d5, do:
     h(decision_test(resource_call(:language_available), true, :v3e5, 406))
-  ## Accept-Charset exists?
+  @doc "Accept-Charset exists?"
   decision v3e5 do
     case h(get_header_val("accept-charset")) do
       nil -> h(decision_test(h(choose_charset("*")),nil, 406, :v3f6))
       _ -> d(v3e6)
     end
   end
-  ## Acceptable Charset available?
+  @doc "Acceptable Charset available?"
   decision v3e6, do:
     h(decision_test(h(choose_charset(h(get_header_val("accept-charset")))),nil, 406, :v3f6))
-  ## Accept-Encoding exists?
-  ## also, set content-type header here, now that charset is chosen)
+  @doc """
+  Accept-Encoding exists?
+  also, set content-type header here, now that charset is chosen)
+  """
   decision v3f6 do
     {type,subtype,params} = h(get_metadata(:'content-type'))
     params = (char=h(get_metadata(:'chosen-charset'))) && Dict.put(params,:charset,char) || params
@@ -121,76 +122,75 @@ defmodule Ewebmachine.Core do
       _ -> d(v3f7)
     end
   end
-  ## Acceptable encoding available?
+  @doc "Acceptable encoding available?"
   decision v3f7, do:
     h(decision_test(h(choose_encoding(h(get_header_val("accept-encoding")))), nil, 406, :v3g7))
-  ## "Resource exists?"
+  @doc "Resource exists?"
   decision v3g7 do
-  ## his is the first place after all conneg, so set Vary here
+    ## his is the first place after all conneg, so set Vary here
     vars = h(variances)
     if length(vars)>0, do: h(set_resp_header("Vary",Enum.join(vars,",")))
     h(decision_test(resource_call(:resource_exists), true, :v3g8, :v3h7))
   end
-  ## "If-Match exists?"
+  @doc "If-Match exists?"
   decision v3g8, do:
     h(decision_test(h(get_header_val("if-match")), nil, :v3h10, :v3g9))
-  ## "If-Match: * exists"
+  @doc "If-Match: * exists"
   decision v3g9, do:
     h(decision_test(h(get_header_val("if-match")), "*", :v3h10, :v3g11))
-  ## "ETag in If-Match"
+  @doc "ETag in If-Match"
   decision v3g11 do
     etags = split_quoted_strings(h(get_header_val("if-match")))
     h(decision_test_fn(resource_call(:generate_etag),&(&1 in etags),:v3h10, 412))
   end
-  ## "If-Match exists"
-  ## (note: need to reflect this change at in next version of diagram)
+  @doc "If-Match exists"
   decision v3h7, do:
     h(decision_test(h(get_header_val("if-match")), nil, :v3i7, 412))
-  ## "If-unmodified-since exists?"
+  @doc "If-unmodified-since exists?"
   decision v3h10, do:
     h(decision_test(h(get_header_val("if-unmodified-since")),nil,:v3i12,:v3h11))
-  ## "I-UM-S is valid date?"
+  @doc "I-UM-S is valid date?"
   decision v3h11 do
     iums_date = h(get_header_val("if-unmodified-since"))
     h(decision_test(convert_request_date(iums_date),:bad_date,:v3i12,:v3h12))
   end
-  ## "Last-Modified > I-UM-S?"
+  @doc "Last-Modified > I-UM-S?"
   decision v3h12 do
     req_date = h(get_header_val("if-unmodified-since"))
     req_erl_date = convert_request_date(req_date)
     res_erl_date = resource_call(:last_modified)
     h(decision_test(res_erl_date > req_erl_date,true,412,:v3i12))
   end
-  ## "Moved permanently? (apply PUT to different URI)"
+  @doc "Moved permanently? (apply PUT to different URI)"
   decision v3i4 do
     case resource_call(:moved_permanently) do
       {true, moved_uri} -> h(set_resp_header("Location", moved_uri)); d(respond(301))
       false -> d(v3p3)
     end
   end
-  ## PUT?
+  @doc "PUT?"
   decision v3i7, do:
     h(decision_test(h(method),"PUT",:v3i4,:v3k7))
-  ## "If-none-match exists?"
+  @doc "If-none-match exists?"
   decision v3i12, do:
     h(decision_test(h(get_header_val("if-none-match")),nil,:v3l13,:v3i13))
-  ## "If-None-Match: * exists?"
+  @doc "If-None-Match: * exists?"
   decision v3i13, do:
     h(decision_test(h(get_header_val("if-none-match")), "*", :v3j18, :v3k13))
-  ## GET or HEAD?
+  @doc "GET or HEAD?"
   decision v3j18, do:
     h(decision_test(h(method) in ['GET','HEAD'],true, 304, 412))
-  ## "Moved permanently?"
+  @doc "Moved permanently?"
   decision v3k5 do
     case resource_call(:moved_permanently) do
       {true, moved_uri}-> h(set_resp_header("Location",moved_uri));d(respond(301))
       false -> d(v3l5)
     end
   end
-  ## "Previously existed?"
+  @doc "Previously existed?"
   decision v3k7, do:
     h(decision_test(resource_call(:previously_existed), true, :v3k5, :v3l7))
-  ## "Etag in if-none-match?"
+  @doc "Etag in if-none-match?"
   decision v3k13 do
     etags = split_quoted_strings(h(get_header_val("if-none-match")))
     ## Membership test is a little counter-intuitive here; if the
@@ -198,49 +198,48 @@ defmodule Ewebmachine.Core do
     ## via v3j18.
     h(decision_test_fn(resource_call(:generate_etag),&(&1 in etags),:v3j18,:v3l13))
   end
-  ## "Moved temporarily?"
+  @doc "Moved temporarily?"
   decision v3l5 do
     case resource_call(:moved_temporarily) do
       {true, moved_uri} -> h(set_resp_header("Location", moved_uri));d(respond(307))
       false -> d(v3m5)
     end
   end
-  ## "POST?"
+  @doc "POST?"
   decision v3l7, do:
     h(decision_test(h(method), "POST", :v3m7, 404))
-  ## "IMS exists?"
+  @doc "IMS exists?"
   decision v3l13, do:
     h(decision_test(h(get_header_val("if-modified-since")),nil,:v3m16,:v3l14))
-  ## "IMS is valid date?"
+  @doc "IMS is valid date?"
   decision v3l14 do
     ims_date = h(get_header_val("if-modified-since"))
     h(decision_test(convert_request_date(ims_date),:bad_date,:v3m16,:v3l15))
   end
-  ## "IMS > Now?"
+  @doc "IMS > Now?"
   decision v3l15 do
     now_date_time = :calendar.universal_time
     req_date = h(get_header_val("if-modified-since"))
     req_erl_date = convert_request_date(req_date)
     h(decision_test(req_erl_date > now_date_time,true,:v3m16,:v3l17))
   end
-  ## "Last-Modified > IMS?"
+  @doc "Last-Modified > IMS?"
   decision v3l17 do
     req_date = h(get_header_val("if-modified-since"))
     req_erl_date = convert_request_date(req_date)
     res_erl_date = resource_call(:last_modified)
     h(decision_test(res_erl_date == nil or res_erl_date > req_erl_date,true,:v3m16, 304))
   end
-  ## "POST?"
+  @doc "POST?"
   decision v3m5, do:
     h(decision_test(h(method),"POST",:v3n5,410))
-  ## "Server allows POST to missing resource?"
+  @doc "Server allows POST to missing resource?"
   decision v3m7, do:
     h(decision_test(resource_call(:allow_missing_post),true,:v3n11,404))
-  ## "DELETE?"
+  @doc "DELETE?"
   decision v3m16, do:
     h(decision_test(h(method),"DELETE",:v3m20,:v3n16))
-  ## DELETE enacted immediately?
-  ## Also where DELETE is forced
+  @doc "DELETE enacted immediately?  Also where DELETE is forced"
   decision v3m20 do
     result = resource_call(:delete_resource)
     ## DELETE may have body and TCP connection will be closed unless body is read
@@ -250,10 +249,10 @@ defmodule Ewebmachine.Core do
   end
   decision v3m20b, do:
     h(decision_test(resource_call(:delete_completed),true,:v3o20,202))
-  ## "Server allows POST to missing resource?"
+  @doc "Server allows POST to missing resource?"
   decision v3n5, do:
     h(decision_test(resource_call(:allow_missing_post),true,:v3n11,410))
-  ## "Redirect?"
+  @doc "Redirect?"
   decision v3n11 do
     if resource_call(:post_is_create) do
       new_path = resource_call(:create_path)
@@ -274,10 +273,10 @@ defmodule Ewebmachine.Core do
     end
     d(redirect_helper)
   end
-  ## "POST?"
+  @doc "POST?"
   decision v3n16, do:
     h(decision_test(h(method),"POST",:v3n11,:v3o16))
-  ## Conflict?
+  @doc "Conflict?"
   decision v3o14 do
     case resource_call(:is_conflict) do
       true -> d(respond(409))
@@ -286,11 +285,13 @@ defmodule Ewebmachine.Core do
         d(v3p11)
     end
   end
-  ## "PUT?"
+  @doc "PUT?"
   decision v3o16, do:
     h(decision_test(h(method), "PUT",:v3o14,:v3o18))
-  ## Multiple representations?
-  ## also where body generation for GET and HEAD is done)
+  @doc """
+    Multiple representations?
+    also where body generation for GET and HEAD is done)
+  """
   decision v3o18 do
     final_body = if h(method) in ["GET","HEAD"] do
       if (etag=resource_call(:generate_etag)), do:
@@ -310,10 +311,10 @@ defmodule Ewebmachine.Core do
   
   decision v3o18b, do:
     h(decision_test(resource_call(:multiple_choices), true, 300, 200))
-  ## Response includes an entity?
+  @doc "Response includes an entity?"
   decision v3o20, do:
     h(decision_test(h(has_resp_body), true, :v3o18, 204))
-  ## Conflict?
+  @doc "Conflict?"
   decision v3p3 do
     if resource_call(:is_conflict) do
       d(respond(409))
@@ -323,7 +324,7 @@ defmodule Ewebmachine.Core do
     end
   end
   
-  ## New resource?  (at this point boils down to "has location header")
+  @doc "New resource?  (at this point boils down to \"has location header\")"
   decision v3p11 do
     if h(get_resp_header("Location")) do
       d(respond(201))
