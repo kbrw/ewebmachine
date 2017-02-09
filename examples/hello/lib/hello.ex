c34c1b3fdbf232f9b958d6f49351c0d44775294b
Jean Parpaillon <jean.parpaillon@free.fr>
2017-02-09 10:21:42 +0100

    In example, add uri with post_is_create: true

--- examples/hello/lib/hello.ex
+++ examples/hello/lib/hello.ex
@@ -68,6 +68,20 @@ defmodule Hello do
 	{true, conn, state}
       end
     end
+
+    resource "/new" do %{} after 
+      plug ApiCommon #this is also a plug pipeline
+      allowed_methods do: ["POST"]
+      content_types_accepted do: ['application/json': :from_json]
+      post_is_create do: true
+      defh create_path(conn, state), do: {state.newpath, conn, state}
+      defh from_json do
+	value = conn |> Ewebmachine.fetch_req_body([]) |> Ewebmachine.req_body |> Poison.decode!
+	newpath = "#{:io_lib.format("~9..0b", [:rand.uniform(999999999)])}"
+	_ = Hello.Db.put(value["id"], value)
+	{true, conn, state |> Map.put(:newpath, newpath)}
+      end
+    end    
     
     resource "/static/*path" do %{path: Enum.join(path, "/")} after
       resource_exists do: File.regular?(path(state.path))
