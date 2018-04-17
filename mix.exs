fcec61dd495125581ccde8b70f6d9b728275a999
Jean Parpaillon <jean.parpaillon@free.fr>
2018-04-17 12:16:05 +0200

    Code janitoring

--- mix.exs
+++ mix.exs
@@ -1,38 +1,46 @@
 defmodule Ewebmachine.Mixfile do
   use Mix.Project
 
-  def project do
-    [ app: :ewebmachine,
-      version: "2.1.5",
-      docs: [
-        main: "Ewebmachine",
-        source_url: "https://github.com/awetzel/ewebmachine",
-        source_ref: "master"
-      ],
-      deps: [
-        {:plug, "~> 1.0"},
-        {:cowboy, "~> 1.0", optional: true},
-        {:ex_doc, ">= 0.0.0", only: :dev},
-        {:dialyxir, "~> 0.4", only: [:dev], runtime: false}
-      ],
+  @description """
+  Ewebmachine contains macros and plugs to allow you to compose
+  HTTP decision handlers and run the HTTP decision tree to get
+  your HTTP response. This project is a rewrite for Elixir and
+  Plug of basho webmachine.
+  """
 
-      description: """
-        Ewebmachine contains macros and plugs to allow you to compose
-        HTTP decision handlers and run the HTTP decision tree to get
-        your HTTP response. This project is a rewrite for Elixir and
-        Plug of basho webmachine.
-      """,
-      package: [links: %{"Source"=>"http://github.com/kbrw/ewebmachine",
-                         "Doc"=>"http://hexdocs.pm/ewebmachine"},
-                maintainers: ["Arnaud Wetzel", "Yurii Rashkovskii", "Jean Parpaillon"],
-                licenses: ["MIT"],
-                files: ["lib", "priv", "mix.exs", "README*", "templates", "LICENSE*"]]]
-  end
+  def project, do: [
+    app: :ewebmachine,
+    version: "2.1.5",
+    docs: docs(),
+    deps: deps(),
+    description: @description,
+    package: package()
+  ]
 
-  def application do
-    [ applications: [:plug],
-      mod: {Ewebmachine.App,[]},
-      env: [] ]
-  end
+  def application, do: [
+    applications: [:plug],
+    mod: { Ewebmachine.App, [] },
+    env: []
+  ]
 
+  defp docs, do: [
+    main: "Ewebmachine",
+    source_url: "https://github.com/kbrw/ewebmachine",
+    source_ref: "master"
+  ]
+
+  defp deps, do: [
+    {:plug, "~> 1.0"},
+    {:cowboy, "~> 1.0", optional: true},
+    {:ex_doc, ">= 0.0.0", only: :dev},
+    {:dialyxir, "~> 0.4", only: [:dev], runtime: false}
+  ]
+
+  defp package, do: [
+    links: %{ "Source" => "http://github.com/kbrw/ewebmachine",
+              "Doc" => "http://hexdocs.pm/ewebmachine" },
+    maintainers: ["Arnaud Wetzel", "Yurii Rashkovskii", "Jean Parpaillon"],
+    licenses: ["MIT"],
+    files: ["lib", "priv", "mix.exs", "README*", "templates", "LICENSE*"]
+  ]
 end
